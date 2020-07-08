aff2ab5a002fdd0f368c99017c201dc582dd62a7
Audronf <federicofasitella@gmail.com>
2020-07-08 14:49:13 -0300

    HTTP request

--- Gemfile
+++ Gemfile
@@ -2,3 +2,6 @@ source 'https://rubygems.org'
 
 gem 'telegram-bot-ruby', '~> 0.12.0'
 gem 'dotenv-rails', '2.7.5'
+gem 'nokogiri', '~> 1.10.10'
+gem 'byebug'
+gem 'httparty'
--- gmah.rb
+++ gmah.rb
@@ -1,15 +1,32 @@
 require 'dotenv/load'
+require 'nokogiri'
+require 'open-uri'
 require 'telegram/bot'
+require 'openssl'
+require 'httparty'
+require 'byebug'
 
 token = ENV['TOKEN']
+
+def search(city)
+  url = ENV['URL'] #.gsub 'city', city
+  doc = Nokogiri::HTML(HTTParty.get(url))
+  results = doc.css('list-right-container', 'div')
+end
 
 Telegram::Bot::Client.run(token) do |bot|
   bot.listen do |message|
     case message.text
     when '/top10'
-      bot.api.send_message(chat_id: message.chat.id, text: 'Test')
+      bot.api.send_message(chat_id: message.chat.id, text: top10)
     else
-      bot.api.send_message(chat_id: message.chat.id, text: message.text)
+      response = search(message.text).length
+      bot.api.send_message(chat_id: message.chat.id, text: response)
     end
   end
+end
+
+def top10
+  response = 'Test'
+  response
 end
