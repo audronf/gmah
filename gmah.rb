7c5768c00bb875864be36376f5e677a40276af06
Audronf <federicofasitella@gmail.com>
2020-07-10 22:29:32 -0300

    Search fixed

--- gmah.rb
+++ gmah.rb
@@ -11,22 +11,22 @@ token = ENV['TOKEN']
 
 def search(city)
   @agent = Mechanize.new
-  url = ENV['URL'].dup.gsub! 'city', city
+  url = ENV['URL'].dup.gsub! 'city', city.downcase
   doc = @agent.get url
-  doc.search('posting-card').each do |card|
-    post = card.search('go-to-posting').click
-    Mechanize.back
+  results = []
+  for i in 0..4 do
+    result = {}
+    price = doc.search('.first-price')[i].text
+    next if price.include? 'USD'
+    expenses = doc.search('.expenses')[i].text
+    total_price = expenses != nil ? expenses.gsub!('.', '').gsub!('+ $ ', '').to_i + price.gsub!('.', '').gsub!('$ ', '').to_i : price.gsub!('.', '').gsub!('$ ', '').to_i
+    address = doc.search('.posting-location')[i].text.split(', ')[0].strip.capitalize
+    features = doc.search('.main-features')[i].text.split(' ')[0]
+    url = ENV['BASE_URL'] + doc.search('.posting-title a').attr('href').value
+    result = {price: total_price, address: address, features: features, url: url}
+    results.push(result)
   end
-  'Test' 
-end
-
-def prepare_search_response(doc)
-  results = doc.css('posting-card', 'div')
-  response = {}
-  results.each do |result|
-    result.click
-    price = i
-  end
+  results.sort_by { |i| i[:price] }
 end
 
 Telegram::Bot::Client.run(token) do |bot|
@@ -36,7 +36,10 @@ Telegram::Bot::Client.run(token) do |bot|
       bot.api.send_message(chat_id: message.chat.id, text: top10)
     else
       response = search(message.text)
-      bot.api.send_message(chat_id: message.chat.id, text: response)
+      response.each_with_index do |post, i|
+        text = "#{i + 1})\nPrecio: $#{post[:price]} \nDirección: #{post[:address]} \nTamaño: #{post[:features]} m² \nURL: #{post[:url]}"
+        bot.api.send_message(parse_mode: 'Markdown', chat_id: message.chat.id, text: text)
+      end
     end
   end
 end
