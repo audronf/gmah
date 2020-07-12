338fa8a63443b4faeee172fb65ab11ae4e18083d
Audronf <federicofasitella@gmail.com>
2020-07-11 21:06:06 -0300

    Finishing /top

--- gmah.rb
+++ gmah.rb
@@ -1,50 +1,53 @@
 require 'dotenv/load'
-require 'nokogiri'
-require 'open-uri'
 require 'telegram/bot'
-require 'openssl'
-require 'httparty'
-require 'byebug'
 require 'mechanize'
 
 token = ENV['TOKEN']
 
-def search(city)
+def initialize_agent
   @agent = Mechanize.new
-  url = ENV['URL'].dup.gsub! 'city', city.downcase
-  doc = @agent.get url
+end
+
+def search(*city)
+  initialize_agent
+  url = city.length != 0 ? ENV['URL'].dup.gsub!('city', city.first.downcase) : ENV['TOP_URL']
+  @doc = @agent.get url
+  fetch_response
+end
+
+def fetch_response
   results = []
-  for i in 0..4 do
+  for i in 0..19 do
     result = {}
-    price = doc.search('.first-price')[i].text
+    price = @doc.search('.first-price')[i].text
     next if price.include? 'USD'
-    expenses = doc.search('.expenses')[i].text
+    expenses = @doc.search('.expenses')[i].text
     total_price = expenses != nil ? expenses.gsub!('.', '').gsub!('+ $ ', '').to_i + price.gsub!('.', '').gsub!('$ ', '').to_i : price.gsub!('.', '').gsub!('$ ', '').to_i
-    address = doc.search('.posting-location')[i].text.split(', ')[0].strip.capitalize
-    features = doc.search('.main-features')[i].text.split(' ')[0]
-    url = ENV['BASE_URL'] + doc.search('.posting-title a').attr('href').value
-    result = {price: total_price, address: address, features: features, url: url}
+    address = @doc.search('.posting-location')[i].text.split(', ')[0].strip.capitalize
+    size = @doc.search('.main-features')[i].text.split(' ')[0]
+    url = ENV['BASE_URL'] + @doc.search('.posting-title a')[i].attr('href')
+    result = {price: total_price, address: address, size: size, url: url}
     results.push(result)
   end
   results.sort_by { |i| i[:price] }
 end
 
+def send_message(bot, message, post, i)
+  text = "#{i + 1})\nPrecio: $#{post[:price]} \nDirección: #{post[:address]} \nTamaño: #{post[:size]} m² \nURL: #{post[:url]}"
+  bot.api.send_message(parse_mode: 'Markdown', chat_id: message.chat.id, text: text)
+end
+
 Telegram::Bot::Client.run(token) do |bot|
   bot.listen do |message|
     case message.text
-    when '/top10'
-      bot.api.send_message(chat_id: message.chat.id, text: top10)
+    when '/top'
+      bot.api.send_message(chat_id: message.chat.id, text: 'Buscando los 20 mejores departamentos en Recoleta, Palermo y Belgrano :)')
+      response = search
     else
       response = search(message.text)
-      response.each_with_index do |post, i|
-        text = "#{i + 1})\nPrecio: $#{post[:price]} \nDirección: #{post[:address]} \nTamaño: #{post[:features]} m² \nURL: #{post[:url]}"
-        bot.api.send_message(parse_mode: 'Markdown', chat_id: message.chat.id, text: text)
-      end
+    end
+    response.each_with_index do |post, i|
+      send_message(bot, message, post, i)
     end
   end
-end
-
-def top10
-  response = 'Test'
-  response
 end
